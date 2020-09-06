80162a58543d93d58e2d8be844f166dd88f2236a
Anna Weakclaw <annas.wieclaw@gmail.com>
2020-09-06 19:09:58 +0100

    from two to three

--- hole2/src/main/java/hole2/TakeHomeCalculator.java
+++ hole2/src/main/java/hole2/TakeHomeCalculator.java
@@ -11,38 +11,46 @@ class TakeHomeCalculator {
         this.percent = percent;
     }
 
-    Pair<Integer, String> netAmount(Pair<Integer, String> first, Pair<Integer, String>... rest) {
+    Money netAmount(Money first, Money... rest) {
 
-        List<Pair<Integer, String>> pairs = Arrays.asList(rest);
+        List<Money> monies = Arrays.asList(rest);
 
-        Pair<Integer, String> total = first;
+        Money total = first;
 
-        for (Pair<Integer, String> next : pairs) {
-            if (!next.second.equals(total.second)) {
-                throw new Incalculable();
-            }
-        }
+        total = plus(monies, total);
 
-        for (Pair<Integer, String> next : pairs) {
-            total = new Pair<>(total.first + next.first, next.second);
-        }
+        Double amount = total.value * (percent / 100d);
+        Money tax = new Money(amount.intValue(), first.currency);
 
-        Double amount = total.first * (percent / 100d);
-        Pair<Integer, String> tax = new Pair<>(amount.intValue(), first.second);
-
-        if (!total.second.equals(tax.second)) {
+        if (!total.currency.equals(tax.currency)) {
             throw new Incalculable();
         }
-        return new Pair<>(total.first - tax.first, first.second);
+        return new Money(total.value - tax.value, first.currency);
     }
 
-    static class Pair<A, B> {
-        final A first;
-        final B second;
+    private Money plus(List<Money> monies, Money total) {
+        for (Money next : monies) {
+            total = plus(total, next);
+        }
 
-        Pair(A first, B second) {
-            this.first = first;
-            this.second = second;
+        return total;
+    }
+
+    private Money plus(Money total, Money next) {
+        if (!next.currency.equals(total.currency)) {
+            throw new Incalculable();
+        }
+        total = new Money(total.value + next.value, next.currency);
+        return total;
+    }
+
+    static class Money {
+        final Integer value;
+        final String currency;
+
+        Money(Integer value, String currency) {
+            this.value = value;
+            this.currency = currency;
         }
 
     }
